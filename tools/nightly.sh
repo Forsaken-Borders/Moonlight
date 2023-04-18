dcf49126baa0fff5d97e3f7431f4998ac92b9d76
Lunar Starstrum <lunar@forsaken-borders.net>
2023-04-18 01:04:22 -0500

    Fix env vars

--- tools/nightly.sh
+++ tools/nightly.sh
@@ -6,15 +6,18 @@ set -x
 # Exit on error
 set -e
 
+BRANCH_NAME="$1"
+NUGET_ORG_API_KEY="$2"
+PUSH_COMMIT=0
+
 # The data file `.github/data/commit_marks` is in KEY=VALUE format where:
 # KEY is the branch name
 # VALUE is the last commit from the release workflow.
 # If the branch name is not found in the file, the function saves the current commit to the branch name.
 # The function returns the count between the saved commit and the current commit.
 get_or_create() {
-    local branch_name="$1"
-    local file="$2"
-    local latest_commit=$(git rev-parse "$branch_name")
+    local file=".github/data/commit_marks"
+    local latest_commit=$(git rev-parse "$BRANCH_NAME")
 
     # Checkout the master branch as we're only interested in the data file stored in the master branch
     git checkout master
@@ -26,19 +29,26 @@ get_or_create() {
     touch "$file"
 
     # Search for the branch name in the file
-    grep -q -F "^$branch_name=" "$file" || echo "$branch_name=$latest_commit" >> "$file"
+    if ! grep -q -F "^$BRANCH_NAME=" "$file"; then
+        echo "$BRANCH_NAME=$latest_commit" >> "$file"
+        git config --global user.email "github-actions[bot]@users.noreply.github.com"
+        git config --global user.name "github-actions[bot]"
+        git add .github/data/build_number
+        git commit -m "[ci-skip] Add build hash for '$BRANCH_NAME' branch"
+        PUSH_COMMIT=1
+    fi
 
     # Get the saved commit
-    local existing_run_number=$(grep "^$branch_name=" "$file" | cut -d'=' -f2)
+    local existing_run_number=$(grep "^$BRANCH_NAME=" "$file" | cut -d'=' -f2)
 
     # Switch back to the branch we were on
-    git checkout "$branch_name"
+    git checkout "$BRANCH_NAME"
 
     # Count the commits between the saved commit and the current commit (inclusive)
     echo $(git rev-list --count "$existing_run_number".."$latest_commit")
 }
 
-BUILD_NUMBER=$(printf "%0*d\n" 5 $(get_or_create "${{ github.ref_name }}" ".github/data/commit_marks"))
+BUILD_NUMBER=$(printf "%0*d\n" 5 $(get_or_create))
 echo $BUILD_NUMBER >> $GITHUB_ENV
 
 # Build and package the project
@@ -50,16 +60,14 @@ dotnet pack -o build \
     -p:VersionSuffix="nightly-$BUILD_NUMBER"
 
 # Push if this is a commit to the master branch
-if [[ "${GITHUB_REF##*/}" == "master" ]]; then
-    dotnet nuget push "build/*" -k "${{ secrets.NUGET_ORG_API_KEY }}" -s https://api.nuget.org/v3/index.json
+if [[ "${$BRANCH_NAME##*/}" == "master" ]]; then
+    dotnet nuget push "build/*" -k "$NUGET_ORG_API_KEY" -s https://api.nuget.org/v3/index.json
 
     # Run the Discord tool
-    DISCORD_TOKEN="${{ secrets.DISCORD_TOKEN }}"
-    DISCORD_GUILD_ID="${{ secrets.DISCORD_GUILD_ID }}"
-    DISCORD_CHANNEL_ID="${{ secrets.DISCORD_CHANNEL_ID }}"
-    DISCORD_CHANNEL_TOPIC="${{ secrets.DISCORD_CHANNEL_TOPIC }}"
-    NUGET_URL="${{ secrets.NUGET_URL }}"
-    GITHUB_URL="${{ github.server_url }}/${{ github.repository }}"
-    LATEST_STABLE_VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
     dotnet run --project ./tools/AutoUpdateChannelDescription -p:VersionSuffix="nightly-$BUILD_NUMBER"
+fi
+
+# Push the commit if we made one
+if [[ $PUSH_COMMIT == 1 ]]; then
+    git push
 fi
--- tools/release.sh
+++ tools/release.sh
@@ -6,23 +6,23 @@ set -x
 # Exit on error
 set -e
 
+TAG_NAME="$1"
+NUGET_ORG_API_KEY="$2"
+COMMIT_HASH=$(git rev-parse HEAD)
+
 # Build the project
 mkdir build
 dotnet pack -o build \
     -c Release \
-    -p:Version=${{ github.event.release.tag_name }}
+    -p:Version="$TAG_NAME"
 
 # Push to NuGet
-dotnet nuget push "build/*" -k "${{ secrets.NUGET_ORG_API_KEY }}" -s https://api.nuget.org/v3/index.json
-
-## Update the build hashes
-# Get the commit hash
-commit_hash=$(git rev-parse HEAD)
+dotnet nuget push "build/*" -k "$NUGET_ORG_API_KEY" -s https://api.nuget.org/v3/index.json
 
 # Find the branches that contain the specific commit and iterate through them
-git branch --contains "$commit_hash" | grep -v "detached" | awk '{print $1}' | while read -r branch; do
+git branch --contains "$COMMIT_HASH" | grep -v "detached" | awk '{print $1}' | while read -r branch_name; do
         # If the branch contains the commit, save the new commit hash to the file
-        sed -i "s/^$branch_name=.*$/$branch_name=$commit_hash/" "$file"
+        sed -i "s/^$branch_name=.*$/$branch_name=$COMMIT_HASH/" "$file"
     fi
 done
 
